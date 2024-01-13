7706d052cf72dd820c11baa18386f579ecb48e90
Nanthavath <nanhtavadh@gmail.com>
2024-01-13 18:03:40 +0700

    install firebase package

--- lib/core/DI/service_locator.dart
+++ lib/core/DI/service_locator.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_generate/core/DI/service_locator.config.dart';
+import 'package:flutter_generate/firebase_options.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
@@ -9,5 +11,8 @@ final getIt = GetIt.instance;
 Future<void> configureDependencies() async {
   WidgetsFlutterBinding.ensureInitialized();
   //initialize dependencies
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await getIt.init();
 }
